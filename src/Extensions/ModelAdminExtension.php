ce21666c66848adc9a6c744024505ef4660fb8fd
Nicolaas <github@sunnysideup.co.nz>
2022-01-14 17:51:54 +1300

    WIP

--- src/Extensions/ModelAdminExtension.php
+++ src/Extensions/ModelAdminExtension.php
@@ -6,29 +6,44 @@ use SilverStripe\Core\Extension;
 
 use SilverStripe\Core\Injector\Injector;
 
+use SilverStripe\Core\Config\Config;
+
 use SilverStripe\Forms\GridField\GridField;
+
+use SilverStripe\Admin\ModelAdmin;
 
 use UndefinedOffset\SortableGridField\Forms\GridFieldSortableRows;
 
 class ModelAdminExtension extends Extension
 {
+
+    private static $excluded_models_from_ssu_extension = [];
+
+    private static $excluded_modeladmins_from_ssu_extension = [];
+
     function updateEditForm($form)
     {
         $owner = $this->getOwner();
-        // This check is simply to ensure you are on the managed model you want adjust accordingly
-        $obj = Injector::inst()->get($owner->modelClass);
-        if ($obj) {
-            $dbFields = $obj->stat('db');
-            $sortFields = ['SortNumber', 'Sort'];
-            foreach ($sortFields as $sortField) {
-                if (isset($dbFields[$sortField])) {
-                    $gridField = $form->Fields()->dataFieldByName($this->sanitiseClassNameHelper($owner->modelClass));
-                    // This is just a precaution to ensure we got a GridField from dataFieldByName() which you should have
-                    if ($gridField instanceof GridField) {
-                        $gridField->getConfig()->addComponent(new GridFieldSortableRows($sortField));
-                    }
+        $excludedModelAdmins = Config::inst()->get(ModelAdmin::class, 'excluded_modeladmins_from_ssu_extension');
+        if(! in_array(get_class($owner), $excludedModelAdmins)) {
+            $excludedModels = Config::inst()->get(ModelAdmin::class, 'excluded_models_from_ssu_extension');
+            if(! in_array($owner->modelClass, $excludedModels)) {
+                // This check is simply to ensure you are on the managed model you want adjust accordingly
+                $obj = Injector::inst()->get($owner->modelClass);
+                if ($obj) {
+                    $dbFields = $obj->stat('db');
+                    $sortFields = ['SortNumber', 'Sort'];
+                    foreach ($sortFields as $sortField) {
+                        if (isset($dbFields[$sortField])) {
+                            $gridField = $form->Fields()->dataFieldByName($this->sanitiseClassNameHelper($owner->modelClass));
+                            // This is just a precaution to ensure we got a GridField from dataFieldByName() which you should have
+                            if ($gridField instanceof GridField) {
+                                $gridField->getConfig()->addComponent(new GridFieldSortableRows($sortField));
+                            }
 
-                    break;
+                            break;
+                        }
+                    }
                 }
             }
         }
