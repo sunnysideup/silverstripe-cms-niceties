29fc5acc8396991791fc1749bfc5a341625c4f05
Nicolaas <github@sunnysideup.co.nz>
2022-01-17 17:03:49 +1300

    WIP

--- src/Extensions/ModelAdminExtension.php
+++ src/Extensions/ModelAdminExtension.php
@@ -2,39 +2,33 @@
 
 namespace Sunnysideup\CMSNiceties\Extensions;
 
-use SilverStripe\Core\Extension;
-
-use SilverStripe\Core\Injector\Injector;
-
-use SilverStripe\Core\Config\Config;
-
-use SilverStripe\Forms\GridField\GridField;
-
 use SilverStripe\Admin\ModelAdmin;
-
+use SilverStripe\Core\Config\Config;
+use SilverStripe\Core\Extension;
+use SilverStripe\Core\Injector\Injector;
+use SilverStripe\Forms\GridField\GridField;
 use UndefinedOffset\SortableGridField\Forms\GridFieldSortableRows;
 
 class ModelAdminExtension extends Extension
 {
-
     private static $excluded_models_from_ssu_extension = [];
 
     private static $excluded_modeladmins_from_ssu_extension = [];
 
     private static $sort_fields_from_ssu_extension = ['SortNumber', 'Sort'];
 
-    function updateEditForm($form)
+    public function updateEditForm($form)
     {
         $owner = $this->getOwner();
         $excludedModelAdmins = Config::inst()->get(ModelAdmin::class, 'excluded_modeladmins_from_ssu_extension');
-        if(! in_array(get_class($owner), $excludedModelAdmins)) {
+        if (! in_array(get_class($owner), $excludedModelAdmins, true)) {
             $excludedModels = Config::inst()->get(ModelAdmin::class, 'excluded_models_from_ssu_extension');
-            if(! in_array($owner->modelClass, $excludedModels)) {
+            if (! in_array($owner->modelClass, $excludedModels, true)) {
                 // This check is simply to ensure you are on the managed model you want adjust accordingly
                 $obj = Injector::inst()->get($owner->modelClass);
                 if ($obj) {
                     $dbFields = $obj->stat('db');
-                    $sortFields = Config::inst()->get(ModelAdmin::class, 'sort_fields_from_ssu_extension');;
+                    $sortFields = Config::inst()->get(ModelAdmin::class, 'sort_fields_from_ssu_extension');
                     foreach ($sortFields as $sortField) {
                         if (isset($dbFields[$sortField])) {
                             $gridField = $form->Fields()->dataFieldByName($this->sanitiseClassNameHelper($owner->modelClass));
@@ -52,6 +46,7 @@ class ModelAdminExtension extends Extension
 
         return $form;
     }
+
     //
     // /**
     //  * Sanitise a model class' name for inclusion in a link
@@ -63,5 +58,4 @@ class ModelAdminExtension extends Extension
     {
         return str_replace('\\', '-', $class);
     }
-
 }
