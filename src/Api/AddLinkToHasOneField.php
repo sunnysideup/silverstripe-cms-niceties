60fb377c65697e1a1ba044c7a01544dbfb369ad1
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2022-06-16 04:41:49 +1200

    WIP

---
+++ src/Api/AddLinkToHasOneField.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Sunnysideup\CMSNiceties\Api;
+
+class AddLinkToHasOneField
+{
+    public static function add_link($field, $object)
+    {
+        $dbFieldNameWithID = $field->getName();
+        $dbFieldName = preg_replace('/ID$/', '', $dbFieldNameWithID);
+        $options = $object->stat('has_one');
+        $className = $options[$dbFieldName] ?? '';
+        if($className) {
+            $value = (int) $object->$dbFieldName()->ID;
+            if($value) {
+                $linkedObject = $className::get()->byId($value);
+                if($linkedObject && $linkedObject->hasMethod('CMSEditLink')) {
+                    $link = $linkedObject->CMSEditLink();
+                    if($link) {
+                        $originalDescription = $field->getDescription();
+                        $descriptions = [$originalDescription];
+                        $descriptions[] = '<a href="'.$link.'">edit '.$linkedObject->getTitle().'</a>';
+                        $descriptions = array_filter($descriptions);
+                        $field->setDescription(implode('<br />', $descriptions));
+                    }
+                }
+            }
+        }
+    }
+}
