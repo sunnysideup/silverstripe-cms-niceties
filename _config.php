fcf4b45a6d2951e6f69944f8d90a1866078024c5
Nicolaas <github@sunnysideup.co.nz>
2024-03-11 13:19:02 +1300

    FIX: add vendor-expose

--- _config.php
+++ _config.php
@@ -4,7 +4,8 @@ use SilverStripe\View\Requirements;
 
 use SilverStripe\Control\Director;
 
-Requirements::add_i18n_javascript('sunnysideup:cms-niceties lang/en.js');
+Requirements::javascript('silverstripe/admin:client/dist/js/i18n.js');
+Requirements::add_i18n_javascript('sunnysideup:cms-niceties lang');
 
 if(!Director::isDev()) {
     Director::forceSSL();
