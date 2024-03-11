cb48e6aa10c0c2b8734d5e15961b8a7f4763499d
Nicolaas <github@sunnysideup.co.nz>
2024-03-11 13:58:30 +1300

    FIX: various cms css fixes

--- _config.php
+++ _config.php
@@ -4,9 +4,6 @@ use SilverStripe\View\Requirements;
 
 use SilverStripe\Control\Director;
 
-Requirements::add_i18n_javascript('silverstripe/asset-admin: client/lang');
-Requirements::add_i18n_javascript('sunnysideup/cms-niceties: client/lang');
-
 if(!Director::isDev()) {
     Director::forceSSL();
 }
