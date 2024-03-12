f88d32933b0f99f85bf10d0ce104aafc1bc5b387
Nicolaas <github@sunnysideup.co.nz>
2024-03-12 14:12:56 +1300

    FIX: cleanup

--- client/lang/en.js
+++ client/lang/en.js
@@ -2,8 +2,7 @@ if (typeof ss === 'undefined' || typeof ss.i18n === 'undefined') {
   /* eslint-disable-next-line no-console */
   console.error('Class ss.i18n not defined')
 } else {
-  console.log('Adding dictionary for AssetAdmin.DETAILS')
   ss.i18n.addDictionary('en', {
-    'AssetAdmin.DETAILS': 'Edit Details'
+    'AssetAdmin.DETAILS': 'Edit'
   })
 }
