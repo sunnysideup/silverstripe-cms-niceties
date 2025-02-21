64ddef18a94510b4c28dfaf5a04c90bc838439e4
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2025-02-21 15:28:09 +1300

    FIX: better canView for modeladmin

--- src/Extensions/ModelAdminExtension.php
+++ src/Extensions/ModelAdminExtension.php
@@ -9,6 +9,7 @@ use SilverStripe\Core\Extension;
 use SilverStripe\Core\Injector\Injector;
 use SilverStripe\Forms\GridField\GridField;
 use SilverStripe\Lumberjack\Forms\GridFieldSiteTreeState;
+use SilverStripe\Security\Permission;
 use SilverStripe\Versioned\Versioned;
 use UndefinedOffset\SortableGridField\Forms\GridFieldSortableRows;
 
@@ -25,41 +26,42 @@ class ModelAdminExtension extends Extension
 
     private static $sort_fields_from_ssu_extension = ['SortNumber', 'Sort', 'SortOrder'];
 
+    private static array $assume_to_allow_all = [
+        'ADMIN',
+    ];
+
+    private static int $max_records_to_check_for_can_view = 10000;
+
     public function updateEditForm($form)
     {
         $owner = $this->getOwner();
-        $excludedModelAdmins = Config::inst()->get(ModelAdmin::class, 'excluded_modeladmins_from_ssu_extension');
-        if (! in_array(get_class($owner), $excludedModelAdmins, true)) {
-            $excludedModels = Config::inst()->get(ModelAdmin::class, 'excluded_models_from_ssu_extension');
-            if (! in_array($owner->modelClass, $excludedModels, true)) {
-                // This check is simply to ensure you are on the managed model you want adjust accordingly
-                $obj = Injector::inst()->get($owner->modelClass);
-                if ($obj) {
-                    $sortFields = Config::inst()->get(ModelAdmin::class, 'sort_fields_from_ssu_extension');
-                    $gridField = $form->Fields()->dataFieldByName($this->sanitiseClassNameHelper($owner->modelClass));
-                    if ($gridField instanceof GridField) {
-                        $config = $gridField->getConfig();
-                        $dbFields = $obj->config()->get('db');
-                        foreach ($sortFields as $sortField) {
-                            if (isset($dbFields[$sortField])) {
-                                // This is just a precaution to ensure we got a GridField from dataFieldByName() which you should have
-                                if (! $config->getComponentByType(GridFieldSortableRows::class)) {
-                                    $obj = $owner->modelClass::singleton();
-                                    if ($obj->hasExtension(Versioned::class)) {
-                                        $sorter = (new GridFieldSortableRows($sortField))
-                                            ->setUpdateVersionedStage(Versioned::LIVE);
-                                    } else {
-                                        $sorter = new GridFieldSortableRows($sortField);
-                                    }
-                                    $config->addComponent($sorter);
+        if ($this->IsIncludedInExtension()) {
+            $obj = Injector::inst()->get($owner->modelClass);
+            if ($obj) {
+                $sortFields = $owner->config()->get('sort_fields_from_ssu_extension');
+                $gridField = $form->Fields()->dataFieldByName($this->sanitiseClassNameHelper($owner->modelClass));
+                if ($gridField instanceof GridField) {
+                    $config = $gridField->getConfig();
+                    $dbFields = $obj->config()->get('db');
+                    foreach ($sortFields as $sortField) {
+                        if (isset($dbFields[$sortField])) {
+                            // This is just a precaution to ensure we got a GridField from dataFieldByName() which you should have
+                            if (! $config->getComponentByType(GridFieldSortableRows::class)) {
+                                $obj = $owner->modelClass::singleton();
+                                if ($obj->hasExtension(Versioned::class)) {
+                                    $sorter = (new GridFieldSortableRows($sortField))
+                                        ->setUpdateVersionedStage(Versioned::LIVE);
+                                } else {
+                                    $sorter = new GridFieldSortableRows($sortField);
                                 }
-                                break;
+                                $config->addComponent($sorter);
                             }
+                            break;
                         }
+                    }
 
-                        if ($obj->hasExtension(Versioned::class) && $obj->hasStages() && class_exists(GridFieldSiteTreeState::class)) {
-                            $config->addComponent(new GridFieldSiteTreeState());
-                        }
+                    if ($obj->hasExtension(Versioned::class) && $obj->hasStages() && class_exists(GridFieldSiteTreeState::class)) {
+                        $config->addComponent(new GridFieldSiteTreeState());
                     }
                 }
             }
@@ -81,31 +83,36 @@ class ModelAdminExtension extends Extension
         return str_replace('\\', '-', $class);
     }
 
-    // /**
-    //  * checks if there is only one record and if no further records can be created.
-    //  * If those conditions are true, then redirect immediately to the record
-    //  * for the convenience of the user.
-    //  *
-    //  * @return void
-    //  */
-    // protected function updateList($list)
-    // {
-    //     $owner = $this->getOwner();
-    //     if($owner->getRequest()->param('ModelClass')) {
-    //         $obj = $list->first();
-    //         $link = $owner->getCMSEditLinkForManagedDataObject($obj);
-    //         // die($_SERVER['HTTP_REFERER'] ?? 'xxx');
-    //         if($list->count() === 1) {
-    //             $obj = $list->first();
-    //             if($obj->canCreate() === true) {
-    //                 $link = $owner->getCMSEditLinkForManagedDataObject($obj);
-    //                 if($link && $owner->getRequest()->getURL() !== $link) {
-    //                     if(Director::is_ajax()) {
-    //                         die('<script>window.location.replace("/' . $link . '");</script>');
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
+    protected function updateList(&$list)
+    {
+        $owner = $this->getOwner();
+        if ($this->IsIncludedInExtension()) {
+            if (!Permission::check($owner->config()->get('assume_to_allow_all'))) {
+                $count = $list->count();
+                $limit = $owner->config()->get('max_records_to_check_for_can_view');
+                $ids = [0 => 0];
+                if ($count < $limit) {
+                    foreach ($list as $record) {
+                        if ($record->canView()) {
+                            $ids[] = $record->ID;
+                        }
+                    }
+                }
+                $list = $list->filter(['ID' => $ids]);
+            }
+        }
+    }
+
+    protected function IsIncludedInExtension(): bool
+    {
+        $owner = $this->getOwner();
+        $excludedModelAdmins = $owner->config()->get('excluded_modeladmins_from_ssu_extension');
+        if (! in_array(get_class($owner), $excludedModelAdmins, true)) {
+            $excludedModels = $owner->config()->get('excluded_models_from_ssu_extension');
+            if (! in_array($owner->modelClass, $excludedModels, true)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
