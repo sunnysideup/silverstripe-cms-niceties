e7fd6980b1596afb4e875022958b8fc7d270eac8
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2025-04-10 21:23:06 +1200

    ENH: add the addto righttitle, description, lefttitle options

---
+++ src/Extensions/CMSNicetiesFormFieldExtension.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Sunnysideup\CMSNiceties\Extensions;
+
+use SilverStripe\Core\Extension;
+
+class CMSNicetiesFormFieldExtension extends Extension
+{
+
+    public function AddToDescription(string $description)
+    {
+        $owner = $this->getOwner();
+        $descriptionArray = [$owner->getDescription()];
+        $descriptionArray[] = $description;
+        return $owner->setDescription(implode('<br />', array_filter($descriptionArray)));
+    }
+
+    public function AddToRightTitle(string $description)
+    {
+        $owner = $this->getOwner();
+        $descriptionArray = [$owner->getRightTitle()];
+        $descriptionArray[] = $description;
+        return $owner->setRightTitle(implode('<br />', array_filter($descriptionArray)));
+    }
+
+    public function AddToLeftTitle(string $description)
+    {
+        $owner = $this->getOwner();
+        $descriptionArray = [$owner->getLeftTitle()];
+        $descriptionArray[] = $description;
+        return $owner->setLeftTitle(implode('<br />', array_filter($descriptionArray)));
+    }
+}
