94e72cd193238f8d7a4e70a012b1c1a2bcb26035
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2025-06-26 17:25:11 +1200

    API: get rid of the search customisation

--- client/src/js/types/left-menu-fix.js
+++ client/src/js/types/left-menu-fix.js
@@ -7,19 +7,19 @@
  *
  * @author Tristan Mastrodicasa
  */
-const $ = window.jQuery;
+// const $ = window.jQuery
 
-$.cookie('cms-menu-sticky', 'true', {
-  path: '/',
-  expires: 31,
-});
+// jQuery.cookie('cms-menu-sticky', 'true', {
+//   path: '/',
+//   expires: 31
+// })
 
-$.cookie('cms-panel-collapsed-cms-menu', 'false', {
-  path: '/',
-  expires: 31,
-});
+// jQuery.cookie('cms-panel-collapsed-cms-menu', 'false', {
+//   path: '/',
+//   expires: 31
+// })
 
-$.cookie('cms-panel-collapsed-cms-content-tools-CMSMain', 'false', {
-  path: '/',
-  expires: 31,
-});
+// jQuery.cookie('cms-panel-collapsed-cms-content-tools-CMSMain', 'false', {
+//   path: '/',
+//   expires: 31
+// })
--- client/src/js/types/search.js
+++ client/src/js/types/search.js
@@ -1,45 +1,45 @@
-// import '../../scss/LeftAndMain.scss';
+// // import '../../scss/LeftAndMain.scss';
 
-const $ = window.jQuery
-const NAME = 'sunnysideup/cms-niceties: LeftAndMain'
-const $document = $(document)
+// const $ = window.jQuery
+// const NAME = 'sunnysideup/cms-niceties: LeftAndMain'
+// const $document = $(document)
 
-const init = () => {
-  // console.log(`${NAME} [init]`);
+// const init = () => {
+//   // console.log(`${NAME} [init]`);
 
-  const $searchHolder = $('.search-holder')
-  const $searchHolderShow = $('[name="showFilter"]')
-  const $searchHolderHide = $(
-    '<button type="button" title="Close" aria-expanded="true" class="search-box__close font-icon-cancel btn--no-text btn--icon-lg btn btn-secondary" aria-label="Close"></button>'
-  )
+//   const $searchHolder = $('.search-holder')
+//   const $searchHolderShow = $('[name="showFilter"]')
+//   const $searchHolderHide = $(
+//     '<button type="button" title="Close" aria-expanded="true" class="search-box__close font-icon-cancel btn--no-text btn--icon-lg btn btn-secondary" aria-label="Close"></button>'
+//   )
 
-  const searchHolderToggle = e => {
-    e.preventDefault()
-    e.stopImmediatePropagation()
+//   const searchHolderToggle = e => {
+//     e.preventDefault()
+//     e.stopImmediatePropagation()
 
-    // console.log(`${NAME} | [name="showFilter"]`);
+//     // console.log(`${NAME} | [name="showFilter"]`);
 
-    $searchHolder.toggleClass('grid-field__search-holder--hidden')
-    $searchHolderShow.show()
-  }
+//     $searchHolder.toggleClass('grid-field__search-holder--hidden')
+//     $searchHolderShow.show()
+//   }
 
-  $searchHolderShow.off('click')
-  $searchHolderHide.off('click')
+//   $searchHolderShow.off('click')
+//   $searchHolderHide.off('click')
 
-  $searchHolderShow.on('click', searchHolderToggle)
-  $searchHolderHide.on('click', searchHolderToggle)
+//   $searchHolderShow.on('click', searchHolderToggle)
+//   $searchHolderHide.on('click', searchHolderToggle)
 
-  $('.search-box__cancel').remove()
-  $('.search-box__group').append($searchHolderHide)
+//   $('.search-box__cancel').remove()
+//   $('.search-box__group').append($searchHolderHide)
 
-  // Remove changed class to prevent confirmation dialog
-  $('.cms-edit-form.changed').removeClass('changed')
-}
+//   // Remove changed class to prevent confirmation dialog
+//   $('.cms-edit-form.changed').removeClass('changed')
+// }
 
-// FIX: dirty timeout
-$document.ready(() => {
-  setTimeout(init, 500)
-})
-$document.ajaxComplete(() => {
-  setTimeout(init, 500)
-})
+// // FIX: dirty timeout
+// $document.ready(() => {
+//   setTimeout(init, 500)
+// })
+// $document.ajaxComplete(() => {
+//   setTimeout(init, 500)
+// })
