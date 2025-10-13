f24cad0b36e03dffa1a01143019070e90f4500ab
Nicolaas <github@sunnysideup.co.nz>
2025-10-13 15:01:58 +1300

    ENH: clean up of resetloggin

--- src/Tasks/ResetLogins.php
+++ src/Tasks/ResetLogins.php
@@ -18,44 +18,45 @@ class ResetLogins extends BuildTask
 
     public function run($request)
     {
-        if (! Director::is_cli()) {
-            echo 'This only runs from the command line!';
-            return;
-        }
-        $forreal = (string) $request->getVar('forreal');
-        if ($forreal !== '1') {
-            echo '<h2>Test run only. To run for real add ?forreal=1 to the URL</h2>';
-        }
-        $members = Member::get()->filterAny([
-            'FailedLoginCount:GreaterThan' => 0,
-            'LockedOutUntil:GreaterThan' => '1970-01-01 00:00:00',
-        ]);
-        foreach ($members as $member) {
-            $message = 'Resetting ' . $member->Email;
-            $save = false;
-            DB::alteration_message($message, 'deleted');
-            if ((int) $member->FailedLoginCount > 0.1) {
-                DB::alteration_message(' - resetting failed logins: ' . $member->FailedLoginCount, 'deleted');
-                $member->FailedLoginCount = 0;
-                $save = true;
+        if (Director::is_cli() || Permission::check('ADMIN')) {
+
+            $forreal = (string) $request->getVar('forreal');
+            if ($forreal !== '1') {
+                echo '<h2>Test run only. To run for real add ?forreal=1 to the URL</h2>';
             }
-            if (strtotime($member->LockedOutUntil) > time()) {
-                DB::alteration_message(' - LOCKED! resetting unlock until: ' . $member->LockedOutUntil, 'deleted');
-                $member->LockedOutUntil = null;
-                $save = true;
-            } elseif ($member->LockedOutUntil) {
-                DB::alteration_message(' - already unlocked after: ' . $member->LockedOutUntil, 'changed');
+            $members = Member::get()->filterAny([
+                'FailedLoginCount:GreaterThan' => 0,
+                'LockedOutUntil:GreaterThan' => '1970-01-01 00:00:00',
+            ]);
+            foreach ($members as $member) {
+                $message = 'Resetting ' . $member->Email;
+                $save = false;
+                DB::alteration_message($message, 'deleted');
+                if ((int) $member->FailedLoginCount > 0.1) {
+                    DB::alteration_message(' - resetting failed logins: ' . $member->FailedLoginCount, 'deleted');
+                    $member->FailedLoginCount = 0;
+                    $save = true;
+                }
+                if (strtotime($member->LockedOutUntil) > time()) {
+                    DB::alteration_message(' - LOCKED! resetting unlock until: ' . $member->LockedOutUntil, 'deleted');
+                    $member->LockedOutUntil = null;
+                    $save = true;
+                } elseif ($member->LockedOutUntil) {
+                    DB::alteration_message(' - already unlocked after: ' . $member->LockedOutUntil, 'changed');
+                }
+                if (! $forreal) {
+                    DB::alteration_message(" - not saving changes (test run only)", 'deleted');
+                    continue;
+                }
+                if (! $save) {
+                    DB::alteration_message(' - nothing to change', 'changed');
+                    continue;
+                }
+                DB::alteration_message(' - saving changes', 'added');
+                $member->write();
             }
-            if (! $forreal) {
-                DB::alteration_message(" - not saving changes (test run only)", 'deleted');
-                continue;
-            }
-            if (! $save) {
-                DB::alteration_message(' - nothing to change', 'changed');
-                continue;
-            }
-            DB::alteration_message(' - saving changes', 'added');
-            $member->write();
+        } else {
+            echo 'You must be an ADMIN to run this task.';
         }
     }
 }
